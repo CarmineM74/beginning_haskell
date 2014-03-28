292349af0ec36c31479c3c1e63e03eace83a597e
Carmine Moleti <carmine@moleti.it>
2014-03-28 09:43:28 +0100

    Ex 4.3

--- chapter4/src/Chapter4/Containers.hs
+++ chapter4/src/Chapter4/Containers.hs
@@ -17,6 +17,30 @@ myadjust f k m = M.alter (\case Nothing -> Nothing
                          ) k m
 
 -- Ex 4.3
-classifyClients :: [Client Integer] -> M.Map ClientKind (S.Set (Client Integer))
-classifyClients = undefined
+-- The first should traverse the list element by element, and perform on each element
+-- the classification, decide which of the map items to modify, and then add itself to
+-- the set;
+classifyClients1 :: [Client Integer] -> M.Map ClientKind (S.Set (Client Integer))
+classifyClients1 =  foldr insertClient newMap
+  where
+    newMap = M.fromList $ zip [GovOrgKind,CompanyKind,IndividualKind] [S.empty,S.empty,S.empty]
+    insertClient c m = M.adjust (S.insert c) clientClass m
+      where
+        clientClass = classifyClient c
 
+-- The second should first create lists corresponding to the three different kinds, and at
+-- the end convert those lists to sets and generate the mentioned map from them.
+classifyClients2 :: [Client Integer] -> M.Map ClientKind (S.Set (Client Integer))
+classifyClients2 clients = M.fromList $ zip [GovOrgKind,CompanyKind,IndividualKind] $ map S.fromList [govs,companies,individuals]
+  where
+    (govs,companies,individuals) = classification clients
+
+classification :: [Client Integer] -> ([Client Integer], [Client Integer], [Client Integer])
+classification = foldr f ([],[],[])
+  where
+    f c (gs,cs,is) 
+      | clientClass == GovOrgKind = (c:gs,cs,is)
+      | clientClass == CompanyKind = (gs,c:cs,is)
+      | clientClass == IndividualKind = (gs,cs,c:is)
+      where
+        clientClass = classifyClient c
--- chapter4/src/Chapter4/Model.hs
+++ chapter4/src/Chapter4/Model.hs
@@ -1,18 +1,38 @@
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Chapter4.Model(Person(..),
                       Client(..),
-                      ClientKind(..)
+                      ClientKind(..),
+                      classifyClient,
+                      clients
                      ) where
 
 data Client i = GovOrg  { clientId :: i, clientName :: String }
               | Company { clientId :: i, clientName :: String
                          , person :: Person, duty :: String }
               | Individual { clientId :: i, person :: Person }
-              deriving Show
+              deriving (Show, Ord, Eq)
 
 data Person = Person { firstName :: String, lastName  :: String }
-              deriving Show
+              deriving (Show,Ord,Eq)
 
 data ClientKind = GovOrgKind | CompanyKind | IndividualKind
+              deriving (Show,Ord,Eq)
+
+classifyClient :: Client a -> ClientKind
+classifyClient (GovOrg {..}) = GovOrgKind
+classifyClient (Company {..}) = CompanyKind
+classifyClient (Individual {..}) = IndividualKind
+
+clients :: [Client Integer]
+clients = [GovOrg 1 "NASA", 
+          Company 2 "Google" (Person "Erich" "Smidth") "CEO", 
+          GovOrg 3 "NSA", 
+          Individual 4 (Person "Carmine" "Moleti"), 
+          Company 5 "FrigoCaserta srl" (Person "Paolo" "Salzillo") "CEO", 
+          Company 6 "FrigoCaserta srl" (Person "Gennaro" "Gianoglio") "WareHouse", 
+          Company 7 "FrigoCaserta srl" (Person "Carmine" "Moleti") "CED", 
+          Individual 8 (Person "Anna" "Del Prete"), 
+          Individual 9 (Person "Mr." "X"), 
+          Individual 10 (Person "Enrico" "Moleti")]
+
