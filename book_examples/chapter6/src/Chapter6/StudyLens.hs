990d7debff2fcad462b41308b92d6656d75082dd
Carmine Moleti <carmine@moleti.it>
2014-04-07 18:37:27 +0200

    Ex 6.3

--- book_examples/chapter6/src/Chapter6/StudyLens.hs
+++ book_examples/chapter6/src/Chapter6/StudyLens.hs
@@ -14,5 +14,17 @@ data Person = Person { _firstName :: String, _lastName :: String }
 makeLenses ''Client
 makeLenses ''Person
 
+data TimeMachine = TM {_manufacturer :: String, _year :: Integer, _price :: Double}
+                    deriving (Eq, Show)
 
+myMachines :: [TimeMachine]
+myMachines = [
+              TM "TimeTravellers Inc." 1900 1839.55,
+              TM "Infinite and Beyond Co." 2450 6549.22,
+              TM "Back to the future" 1982 553.66
+            ]
 
+makeLenses ''TimeMachine
+
+increasePriceByPct :: [TimeMachine] -> Double -> [TimeMachine]
+increasePriceByPct machines pct = machines & (traversed.price *~ (1+(pct/100)))
