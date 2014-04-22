ceb94e5706d996814fdb45df85c9a93f965f60dd
Carmine Moleti <carmine.moleti@gmail.com>
2014-04-22 07:08:31 +0200

    Ex 6.6

---
+++ book_examples/chapter6/src/StudyReaderWriter.hs
@@ -0,0 +1,13 @@
+module Chapter6.StudyReaderWriter where
+
+import Data.Monoid
+
+newtype MyWriter m a = MyWriter (a,m)
+
+instance Monoid m => Monad (MyWriter m) where
+  -- return :: a -> MyWriter m a
+  return x = MyWriter (x,mempty)
+  -- >>= :: (MyWriter m a) -> (a -> MyWriter m b) -> (Mywriter m b)
+  (>>=) (MyWriter (x,s)) f = MyWriter (y,mappend s s')
+                            where
+                              MyWriter (y,s') = f x 
